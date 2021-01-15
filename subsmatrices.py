53e4cf5c57a1f08ee20d79847229477a393a265f
Karina Halevy <karina.halevy@gmail.com>
2021-01-15 00:54:50 -0800

    initial file upload

---
+++ subsmatrices.py
@@ -0,0 +1,62 @@
+import io
+import numpy as np
+import nltk
+from nltk.stem.porter import *
+from nltk.stem.snowball import SnowballStemmer
+import string
+from nltk.stem import WordNetLemmatizer
+import math
+from nltk.corpus import stopwords
+import fasttext
+import fasttext.util
+import sys
+
+lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
+snowball = SnowballStemmer("porter")
+sl = ['arabic', 'english', 'dutch', 'portuguese', 'spanish', 'danish', 'finnish',
+'french', 'german', 'hungarian', 'italian', 'norwegian', 'romanian', 'russian',
+'swedish']
+
+path_suffix = '.vec'
+language_paths = ['ar', 'bg', 'ca', 'hr', 'cs', 'da', 'nl', 'en', 'et', 'fi',
+                  'fr', 'el', 'he', 'hu', 'id', 'it', 'mk', 'no', 'pl',
+                  'pt', 'ro', 'ru', 'sk', 'sl', 'es', 'sv', 'tr', 'uk', 'vi']
+languages = ['arabic', 'bulgarian', 'catalan', 'croatian', 'czech', 'danish',
+              'dutch', 'english', 'estonian', 'finnish', 'french',
+              'greek', 'hebrew', 'hungarian', 'indonesian', 'italian',
+              'macedonian', 'norwegian', 'polish', 'portuguese', 'romanian',
+              'russian', 'slovak', 'slovenian', 'spanish', 'swedish',
+              'turkish', 'ukrainian', 'vietnamese']
+word_files = [i + '.txt' for i in languages]
+
+def get_nn_words(word, model):
+  nns = []
+  m = list(model.get_nearest_neighbors(word, k = 1000000))
+  for s, w in m:
+    nns.append(w)
+  return nns
+
+i = int(sys.argv[1])
+print(languages[i])
+mat = []
+model = fasttext.load_model('subs2vec/subs.' + language_paths[i] + '.bin')
+with open(word_files[i], "r") as words:
+  wds = [line.lower().strip("\n") for line in words]
+for j in range(1):
+  neighbs = get_nn_words(wds[j], model)
+  vals = []
+  '''
+  for n in range(len(wds)):
+    try:
+      k = neighbs.index(wds[n])
+    except:
+      k = -1
+    vals.append(k)
+  mat.append(vals)
+with open(languages[i] + "matrixsubs500.txt", "w") as o:
+  for q in range(len(wds)):
+    o.write(str(mat[q]) + "\n")
+print("done with" + languages[i])
+'''
+
