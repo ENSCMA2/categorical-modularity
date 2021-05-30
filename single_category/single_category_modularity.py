647d4e07e0720ee976d79ff62cbc84c086ad4c2c
Karina Halevy (she/her/hers) <karina.halevy@gmail.com>
2021-05-29 21:18:06 -0700

    spacing

--- single_category/single_category_modularity.py
+++ single_category/single_category_modularity.py
@@ -15,12 +15,12 @@ parser.add_argument("--categories_file",
     help = "name of file with category labels, csv, no headers",
     default = "data/categories_3.csv")
 parser.add_argument("--matrix_file",
-    help = "name of csv file with square k-NN matrix",
+    help = "name of csv file with square k-NN matrix, no labels",
     default = "data/muse_finnish.csv")
 args = parser.parse_args()
 
-file = open(args.matrix_file)  # each language, upload without the category words; LANGUAGE
-M = np.loadtxt(file, delimiter=",")
+file = open(args.matrix_file)
+M = np.loadtxt(file, delimiter = ",")
 
 categories = []
 with open(args.categories_file) as csvfile:    
@@ -44,7 +44,7 @@ counters.append(i)  # for the last category
 lst = [2, 3, 4]
 for k in lst:
     categories_modularity = []
-    knn = kneighbors_graph(M, k, mode='connectivity', include_self=True) 
+    knn = kneighbors_graph(M, k, mode = 'connectivity', include_self = True) 
     knnmatrix = knn.toarray()
 
     # counting a_c (modularity paper notation, changing l of languages for c of categories)
@@ -52,7 +52,7 @@ for k in lst:
     t = 0
     for c in counters:  # for each category
         cjtotal = 0
-        for i in range(t, t+c):  # how many words in that category
+        for i in range(t, t + c):  # how many words in that category
             for j in knnmatrix[i]:  # count degree of that word / node
                 if (j == 1):
                     cjtotal += 1
@@ -68,15 +68,15 @@ for k in lst:
 
     # now the true ac
     for i in range(len(ac)):
-        ac[i] = ac[i]/(m)
+        ac[i] = ac[i] / m
 
     # now we compute ell (modularity paper), called ecc here (fraction of edges within the same category)
     ecc = []
     t = 0
     for c in counters:
         ecctotal = 0
-        for i in range(t, t+c):
-            for j in range(t, t+c):
+        for i in range(t, t + c):
+            for j in range(t, t + c):
                 if (knnmatrix[i, j] == 1):
                     ecctotal += 1
         ecc.append(ecctotal)
@@ -84,20 +84,20 @@ for k in lst:
 
     # actually divided by 2m
     for i in range(len(ecc)):
-        ecc[i] = ecc[i]/(m)
+        ecc[i] = ecc[i] / m
 
     # Given C total categories, we calculate the overall modularity Q
     Q = 0
     for i in range(len(counters)):
-        Q += ecc[i] - ac[i]*ac[i]
+        Q += ecc[i] - ac[i] * ac[i]
         categories_modularity.append(ecc[i] - ac[i] * ac[i])  # currently not normalized
   
     # finally, we normalize
     Qmax = 0
     for i in range(len(counters)):
-        Qmax += ac[i]*ac[i]
+        Qmax += ac[i] * ac[i]
     Qmax = 1 - Qmax
-    Qnorm = Q/Qmax
+    Qnorm = Q / Qmax
     for i in range(len(counters)):
         categories_modularity[i] = categories_modularity[i] / Qmax
     # if we want it normalized, divide every element in the array by Qnorm
